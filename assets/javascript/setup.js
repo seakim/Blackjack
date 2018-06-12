08f8d562490b95fa095ebf08bfc32fdee6f16419
Sean Kim <dev.seakim@gmail.com>
2018-06-12 17:13:39 -0400

    working on dealing cards

--- assets/javascript/setup.js
+++ assets/javascript/setup.js
@@ -1,15 +1,15 @@
 //
 // Sean Kim
 //
-// Setting up a set of suffled 8 decks to be ready to play
 
+
+// Setting up a set of suffled 8 decks to be ready to play
 $(document).ready(function () {
 
     var unshuffledDeck = [],
         shuffledDeck = [],
-        dealtCard = "",
-        click = 1;
-
+        dealtCard = "";
+        cardValue = 0;
     // Set a set of 8 decks (416 cards); unshuffledDeck[i][j]
     for (var k = 0; k < 8; k++) {
 
@@ -28,7 +28,6 @@ $(document).ready(function () {
 
     // 8 decks shuffled; shuffledDeck[i][0][j]
     // shuffledDeck = [ [  [x1,y1] ],[ [x2,y2] ],[ [x3,y3] ] ... [ [x,y] ] ]
-
     for (var i = 0; i < 416; i++) {
         var rand = Math.floor(Math.random() * 416 - i);
         shuffledDeck.push(unshuffledDeck.splice(rand, 1));
@@ -55,30 +54,53 @@ $(document).ready(function () {
     // console.log(c3);
 
 
-    // Dealing
-    $(".cards_area").on("click", ".player", function () {
-        dealtCard = shuffledDeck.shift()[0];
+
+    // Dealing first cards
+    // Dealing second cards; second card for dealer is face down;
+    
+    function deal() {
+        for (var i = 0; i < $(".hands_area").length; i++) {
+            dealtCard = shuffledDeck.shift()[0];
+
+            // get the card value
+            // console.log(dealtCard[0]);
+            // cardValue += dealtCard[0] + 1;
+            // $(".hands_area:eq(" + i + ")").attr("value", cardValue);
+
+            // display cards
+            var cardH = "<div class='cards'><img id='"
+            if (dealtCard[1] === 0) {
+                cardH += "c";
+            } else if (dealtCard[1] === 1) {
+                cardH +="s";
+            } else if (dealtCard[1] === 2) {
+                cardH +="h";
+            } else if (dealtCard[1] === 3) {
+                cardH += "d";
+            }
+            cardH += (dealtCard[0] + 1);
+            cardH += "' src='assets/images/deck.png' /></div>";
+            console.log(cardH);
+            $(".hands_area:eq(" + i + ")").delay(2000).append(cardH);
+        }
+    }
+    
+    function gameStart() {
+        deal();
+        deal();
+        console.log(shuffledDeck.length);
+    }
+    $(".deal").on("click", function (){
+        gameStart();
+    });
+
 
         // <div class='card1 cards'>
         //     <img id="d13" src="assets/images/deck.png" />
         // </div>
-        var cardH = "<div class='card" + click + " cards'><img id='"
-        if (dealtCard[1] === 0) {
-            cardH += "c";
-        } else if (dealtCard[1] === 1) {
-            cardH +="s";
-        } else if (dealtCard[1] === 2) {
-            cardH +="h";
-        } else if (dealtCard[1] === 3) {
-            cardH += "d";
-        }
-        cardH += (dealtCard[0] + 1);
-        cardH += "' src='assets/images/deck.png' /></div>";
-        console.log(cardH);
-        $(".player").append(cardH);
-        console.log(shuffledDeck.length);
-        click++;
-    });
+        // deal first card
+
+
 
 
 
